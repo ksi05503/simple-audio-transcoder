3c637348ce5e28d06c13e3b09039b12ac8bb4704
김현웅 <ksi05503@gmail.com>
2023-05-05 19:56:04 +0900

    chore: tsconfig and jest config

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from '@jest/types';
 
 const config: Config.InitialOptions = {
+    roots: ['<rootDir>/__tests__'],
     preset: 'ts-jest',
     testEnvironment: 'node',
     testMatch: ['**/__tests__/**/*.test.[jt]s?(x)'],
